5ebed660e43be9381f5810fd730cbfd49719e369
Igor Dziuba <cherobuk.91@gmail.com>
2022-06-12 00:15:01 +0300

    Added model WorkExperience, made management through the admin panel. Added ckeditor and added data for output to the site (Work experiences)

--- django_portfolio/portfolio/admin.py
+++ django_portfolio/portfolio/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django import forms
+from .models import WorkExperience
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+
+class WorkExperienceAdminForm(forms.ModelForm):
+    responsibilities = forms.CharField(label='Обязанности', widget=CKEditorUploadingWidget())
+    class Meta:
+        model = WorkExperience
+        fields = '__all__'
+
 
 # Register your models here.
+@admin.register(WorkExperience)
+class WorkExperienceAdmin(admin.ModelAdmin):
+  '''Опыт работы'''
+  list_display = ("start_date","end_date","company_name","position","responsibilities")
+  form = WorkExperienceAdminForm
+  search_fields = ("company_name",)
--- django_portfolio/portfolio/models.py
+++ django_portfolio/portfolio/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class WorkExperience(models.Model):
+  '''Опыт работы'''
+  start_date = models.DateField("Начало работы", auto_now=False, auto_now_add=False)
+  end_date = models.DateField("Окончание", auto_now=False, auto_now_add=False, blank=True, null=True)
+  company_name = models.CharField("Название компании", max_length=100)
+  position = models.CharField("Должность", max_length=150)
+  responsibilities = models.TextField("Обязанности", max_length=2500)
+
+  def __str__(self):
+    return f'{self.company_name}: {self.start_date} - {self.end_date}'
